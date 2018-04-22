f7111b0b223981adf93fb3bc20d65de88469c5b9
Lamido Tijjani <kinglamido@gmail.com>
2018-04-22 20:22:09 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,31 +1,22 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-var index = 0;
-
+let id = 0;
 function init() {
-  // your code here
-  var konami = document.querySelector('body');
-  
-  konami.addEventListener('keydown', function(e) {
-
-    const key = parseInt(e.detail || e.which)
-    
-    for (var i = 0; i < code.length; i++) {
-      if(key === code[i]) {
-          index += 1;
-          if(index === code.length-1)
-          alert("YOU DID IT");
-          index=0;
-    } else {
-      alert("Wrong Code")
-      index=0;
+const konamiCode = document.querySelector('body')
+konamiCode.addEventListener('keydown', function(e){
+ const key = parseInt(e.detail || e.which);
+   for(let i = 0; i < code.length; i++){
+    if (key === code[i]) {
+    id += 1
+ 
+    if (id === code.length - 1 ) {
+      alert("wow coder!");
+ 
+      id = 0;
     }
-
+  } else {
+    id = 0;
   }
-  return index;
-
-
-  })
+} return id
+})
 }
-
-init();
